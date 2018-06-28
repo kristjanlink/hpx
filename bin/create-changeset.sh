19d739c4ffe7d0fa2663e89e89b054ee0d5edc2a
agcastel <adolfoc@gmail.com>
2018-06-28 12:22:11 -0700

    Ok NOW create-changeset respects both branch and environment properly

--- bin/create-changeset.sh
+++ bin/create-changeset.sh
@@ -2,5 +2,20 @@
 # ./create-change-set.sh <stack name>
 BRANCH=`git rev-parse --abbrev-ref HEAD`
 LUSER=`whoami`
+ENVIRONMENT="prod"
 
-aws cloudformation create-change-set --stack-name hpx-$BRANCH --template-url https://s3-us-west-2.amazonaws.com/hpx-code/$BRANCH/cloudformation/hpx.yaml --change-set-name hpx-manual-changeset-$LUSER
+if [ $BRANCH != "master" ]; then
+  ENVIRONMENT="dev"
+fi
+
+echo "Creating changeset for stack hpx-$BRANCH"
+echo "BRANCH=$BRANCH, ENVIRONMENT=$ENVIRONMENT"
+
+aws cloudformation create-change-set \
+  --stack-name hpx-$BRANCH \
+  --template-url https://s3-us-west-2.amazonaws.com/hpx-code/$BRANCH/cloudformation/hpx.yaml \
+  --change-set-name hpx-manual-changeset-$LUSER \
+  --parameters \
+ParameterKey=\"Environment\",\
+ParameterValue=\"$ENVIRONMENT\",\
+UsePreviousValue=false\
