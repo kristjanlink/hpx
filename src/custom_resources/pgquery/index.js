cc54560246d0d74e8a00f8111a193794e64704bd
agcastel <adolfoc@gmail.com>
2018-07-10 10:01:06 -0700

    Cleaning up code after tracking down timeout issue to NAT configuration

--- src/custom_resources/pgquery/index.js
+++ src/custom_resources/pgquery/index.js
@@ -3,6 +3,11 @@ const { Client } = require('pg');
 
 exports.handler = function(event, context) {
   context.callbackWaitsForEmptyEventLoop = false;
+  console.log( JSON.stringify({
+    RequestType: event.RequestType,
+    ResponseURL: event.ResponseURL
+  }));
+
   if (event.RequestType == "Create" || event.RequestType == "Update") {
     var client = new Client({
       user: event.ResourceProperties.User,
@@ -11,40 +16,33 @@ exports.handler = function(event, context) {
       password: event.ResourceProperties.Password,
       port: event.ResourceProperties.Port,
     });
+
     client.connect((err) => {
       if (err) {
         console.log("Connection Error:", err, err.stack);
         response.send(event, context, response.FAILED);
-        return;
       } else {
         console.log("Connected:");
         client.query(event.ResourceProperties.Query, (err, res) => {
+          var status = response.SUCCESS;
           if (err) {
             console.log("Error:", err, err.stack);
-            client.end((err) => {
-              console.log('client has disconnected');
-              if (err) {
-                console.log('error during disconnection', err.stack);
-              };
-              response.send(event, context, response.FAILED);
-              return;
-            });
+            status = response.FAILED;
           } else {
             console.log("Success:", res.rows);
-            client.end((err) => {
-              console.log('client has disconnected');
-              if (err) {
-                console.log('error during disconnection', err.stack);
-              };
-              response.send(event, context, response.SUCCESS);
-              return;
-            });
           }
+          client.end((err) => {
+            console.log("Client has disconnected");
+            if (err) {
+              console.log("Error during disconnection", err.stack);
+            };
+            response.send(event, context, status);
+          });
         });
       }
     });
   } else {
     response.send(event, context, response.SUCCESS);
-    return
+    return;
   }
 };
