4395ebceb86cfe583dbc1c6d8cfeabe238e322ff
Dan Hsiung <danhsiung@gmail.com>
2018-07-12 12:43:47 -0700

    got trucation working

--- src/custom_resources/s3cleanup/index.js
+++ src/custom_resources/s3cleanup/index.js
@@ -2,42 +2,49 @@ var response = require('cfn-response');
 var aws = require('aws-sdk');
 var s3 = new aws.S3();
 
+var allKeys = [];
+function listAllKeys(s3bucket, token, cb) {
+  var opts = { Bucket: s3bucket, MaxKeys: 2 };
+  if(token) opts.ContinuationToken = token;
+
+  s3.listObjectsV2(opts, function(err, data) {
+    if (err) {
+      cb(err, data);
+    } else {
+      allKeys = allKeys.concat(data.Contents);
+      if(data.IsTruncated) {
+        listAllKeys(s3bucket, data.NextContinuationToken, cb);
+      } else {
+        cb(err, allKeys);
+      }
+    }
+  });
+}
+
 exports.handler = function(event, context) {
-  if (event.RequestType == "Delete") {
-    var truncated = true;
-    var error = false;
-    //while (truncated && !error) {
-      let list = s3.listObjectsV2( {
-        "Bucket": event.ResourceProperties.Bucket
-      }, function(err, data) {
-        if (err) {
-          error = true;
-          console.log("Error:", err, err.stack);
-          response.send(event, context, response.FAILED)
-        } else {
-          truncated = data.IsTruncated;
-          console.log("Listed keys for bucket!", truncated);
-          let objects = [];
-          console.log("Contents:", data.Contents);
-          data.Contents.forEach( function(obj) {
-            console.log("Found key to delete:", obj.Key);
-            objects.push( { "Key": obj.Key } );
-          });
-          let del_request = s3.deleteObjects( {
-            "Bucket": event.ResourceProperties.Bucket,
-            "Delete": { "Objects": objects }
-          }, function(err, data) {
-            if (err) {
-              error = true;
-              console.log("Error:", err, err.stack);
-              response.send(event, context, response.FAILED)
-            } else {
-              console.log("Successful delete:", data);
-              response.send(event, context, response.SUCCESS, data)
-            }});
-        }
-      });
-    //}
+  if (true) { //event.RequestType == "Delete") {
+    listAllKeys(event.ResourceProperties.Bucket, false, function(err, data) {
+      if (err) {
+        console.log("Error:", err, err.stack);
+        response.send(event, context, response.FAILED)
+      } else {
+        var objects = [];
+        data.forEach( function(obj) {
+          objects.push( { "Key": obj.Key } );
+        });
+        let del_request = s3.deleteObjects( {
+          "Bucket": event.ResourceProperties.Bucket,
+          "Delete": { "Objects": objects }
+        }, function(err, data) {
+          if (err) {
+            console.log("Error:", err, err.stack);
+            response.send(event, context, response.FAILED)
+          } else {
+            console.log("Successful delete:", data);
+            response.send(event, context, response.SUCCESS, data)
+          }});
+      }
+    });
   } else {
     response.send(event, context, response.SUCCESS, {})
   }
