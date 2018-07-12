48b5cbb1757992aa1fa904ebe272be7113e4eb01
Dan Hsiung <danhsiung@gmail.com>
2018-07-12 10:22:20 -0700

    code fixes

--- src/custom_resources/s3cleanup/index.js
+++ src/custom_resources/s3cleanup/index.js
@@ -6,7 +6,7 @@ exports.handler = function(event, context) {
   if (event.RequestType == "Delete") {
     var truncated = true;
     var error = false;
-    while (truncated && !error) {
+    //while (truncated && !error) {
       let list = s3.listObjectsV2( {
         "Bucket": event.ResourceProperties.Bucket
       }, function(err, data) {
@@ -16,27 +16,27 @@ exports.handler = function(event, context) {
           response.send(event, context, response.FAILED)
         } else {
           truncated = data.IsTruncated;
+          console.log("Listed keys for bucket!", truncated);
+          let objects = [];
           for (let o in data.Contents) {
-            let del_request = s3.deleteObject( {
-              "Bucket": event.ResourceProperties.Bucket,
-              "Key": o.Key
-            }, function(err, data) {
-              if (err) {
-                error = true;
-                console.log("Error:", err, err.stack);
-                response.send(event, context, response.FAILED)
-              } else {
-                console.log("Successful delete:", data);
-              }
-            });
+            console.log("Found key to delete:", o.Key);
+            objects.push( { "Key": o.Key } );
           }
+          let del_request = s3.deleteObjects( {
+            "Bucket": event.ResourceProperties.Bucket,
+            "Objects": objects
+          }, function(err, data) {
+            if (err) {
+              error = true;
+              console.log("Error:", err, err.stack);
+              response.send(event, context, response.FAILED)
+            } else {
+              console.log("Successful delete:", data);
+              response.send(event, context, response.SUCCESS, data)
+            }});
         }
       });
-      if (!error) {
-        console.log("Done cleaning up bucket:", event.ResourceProperties.Bucket);
-        response.send(event, context, response.SUCCESS, {})
-      }
-    }
+    //}
   } else {
     response.send(event, context, response.SUCCESS, {})
   }
